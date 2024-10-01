dd82a8c80430d4d1ac8e81d95d47b7469f14ef6c
Mfon Eti-mfon <mfonetimfon@gmail.com>
2024-10-01 22:56:04 +0100

    Add email validation to signup flow

--- src/API/Signup/Handler.hs
+++ src/API/Signup/Handler.hs
@@ -2,7 +2,12 @@ module API.Signup.Handler (signupHandler) where
 
 import API.Signup.Request (SignupRequest (..))
 import API.Signup.Response (SignupResponse (..))
+import Data.ByteString.Char8 as B8
 import Servant (Handler)
+import Text.Email.Validate qualified
 
 signupHandler :: SignupRequest -> Handler SignupResponse
-signupHandler (SignupRequest email) = return $ SignupResponse email True
+signupHandler (SignupRequest email) =
+  return $ case Text.Email.Validate.validate (B8.pack email) of
+    Left _ -> SignupResponse email False
+    Right _ -> SignupResponse email True
--- test/API/Signup/HandlerSpec.hs
+++ test/API/Signup/HandlerSpec.hs
@@ -5,12 +5,19 @@ module API.Signup.HandlerSpec where
 import API.Signup.Request (SignupRequest (..))
 import API.Signup.Response (SignupResponse (..))
 import App (app)
+import Control.Monad (forM_)
 import Network.HTTP.Types (status200)
 import Test.Hspec
 import TestUtils (assertJsonContentType, assertStatus, decodeJsonResponse, runPostRequest)
 
 handlerSpec :: Spec
-handlerSpec = describe "POST /signup" $ do
+handlerSpec =
+  describe "POST /signup" $ do
+    handleSignupForValidEmail
+    handleSignupForInvalidEmail
+
+handleSignupForValidEmail :: Spec
+handleSignupForValidEmail =
   it "responds with JSON indicating that a signup request has been addressed" $ do
     let signupRequest = SignupRequest "magic@link.poof"
     response <- runPostRequest "/signup" signupRequest app
@@ -23,3 +30,35 @@ handlerSpec = describe "POST /signup" $ do
         { email = "magic@link.poof",
           magicLinkSent = True
         }
+
+handleSignupForInvalidEmail :: Spec
+handleSignupForInvalidEmail =
+  describe "with invalid email addresses" $ do
+    let invalidEmails =
+          [ "plainaddress", -- Missing @ symbol
+            "@example.com", -- Missing local part (username) before @
+            "john@@example.com", -- Multiple @ symbols
+            ".john@example.com", -- Leading dot in the local part
+            "john.@example.com", -- Trailing dot in the local part
+            "john..doe@example.com", -- Consecutive dots in the local part
+            "john.doe@example.com#", -- Invalid character (#) in the local part
+            "john.doe@", -- Missing domain part after @
+            "john doe@example.com", -- Space in the email address
+            "john.doe@example!com", -- Invalid character (!) in the domain part
+            "john.doe@.example.com" -- Domain starts with a dot
+          ]
+
+    forM_ invalidEmails $ \invalidEmail -> do
+      it "responds with JSON indicating that magic link wasn't sent" $ do
+        let signupRequest = SignupRequest invalidEmail
+
+        response <- runPostRequest "/signup" signupRequest app
+        assertStatus status200 response
+        assertJsonContentType response
+
+        decodedResponse <- decodeJsonResponse response
+        decodedResponse
+          `shouldBe` SignupResponse
+            { email = invalidEmail,
+              magicLinkSent = False
+            }
