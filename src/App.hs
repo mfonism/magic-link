4a18d6d056ef0e3051078b80c1b7c046cf73a465
Mfon Eti-mfon <mfonetimfon@gmail.com>
2024-10-15 00:46:34 +0100

    Prune out unused code

--- src/App.hs
+++ src/App.hs
@@ -1,20 +1,8 @@
-module App (mkApp, HelloResponse (..)) where
+module App (mkApp) where
 
 import App.Context (AppContext)
-import Data.Aeson (FromJSON, ToJSON)
-import Data.Text (Text)
-import GHC.Generics (Generic)
-import Servant
+import Servant (Application, Proxy (..), serve)
 import Server qualified
-
-newtype HelloResponse = HelloResponse
-  { message :: Text
-  }
-  deriving (Show, Eq, Generic)
-
-instance ToJSON HelloResponse
-
-instance FromJSON HelloResponse
 
 mkApp :: AppContext -> Application
 mkApp appCtx = serve (Proxy :: Proxy Server.API) $ Server.mkServer appCtx
